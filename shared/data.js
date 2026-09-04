e280298dc1e0379841d6ed5a93c4e3b79f1a743f
7j22g5cgbk-svg <7j22g5cgbk@privaterelay.appleid.com>
2026-09-05 00:17:00 +0200

    Library Update

--- shared/data.js
+++ shared/data.js
@@ -1,24 +1,121 @@
 window.SHARED_LIBRARY = {
-  "hotels": [],
+  "hotels": [
+    {
+      "id": "7ildcvsqmtmwj49w",
+      "name": "Maison Breguet",
+      "city": "Paris",
+      "note": "Location great, clean, nice design, swimming pool",
+      "website": "https://maisonbreguet.com",
+      "price": ""
+    },
+    {
+      "id": "7mrkg3abmtmwjx9n",
+      "name": "August",
+      "city": "Antwerp",
+      "note": "Former Monastery, beautiful design, a bit outside of the city",
+      "website": "https://august-antwerp.com",
+      "price": ""
+    },
+    {
+      "id": "vr5hahyzmtmwls6t",
+      "name": "Prince Akatoki",
+      "city": "London",
+      "note": "Book early",
+      "website": "https://theprinceakatokilondon.com",
+      "price": ""
+    },
+    {
+      "id": "qlqcvs3gmtmwr0dg",
+      "name": "Villa Auersperg",
+      "city": "Salzburg",
+      "note": "Best Value for Money",
+      "website": "https://www.auersperg.at",
+      "price": ""
+    }
+  ],
   "restaurants": [
     {
       "id": "hboou29smtjwdikb",
       "name": "Palacio Do Grilo",
       "city": "Lisbon",
-      "note": "",
-      "website": "",
-      "cuisine": ""
+      "note": "Asylum / Performance / Cabaret",
+      "website": "https://palaciogrilo.com",
+      "cuisine": "Modern European"
     },
     {
       "id": "yr0hh7romtjwfy9g",
       "name": "Gymkhana",
       "city": "London",
+      "note": "Reserve well in advance",
+      "website": "https://gymkhanarestaurants.com/",
+      "cuisine": "Indian, Michelin Star"
+    },
+    {
+      "id": "p4t9hjkomtmw6xbz",
+      "name": "Satan`s Whiskers",
+      "city": "London",
+      "note": "Best Bar",
+      "website": "https://www.satanswhiskers.com",
+      "cuisine": "Bar"
+    },
+    {
+      "id": "5h1mq0qimtmw8ydu",
+      "name": "Lore of the Land",
+      "city": "London",
+      "note": "Owned by Guy Richie",
+      "website": "https://loreoftheland.com",
+      "cuisine": "Gastro Pub great meat"
+    },
+    {
+      "id": "wr6gn8xfmtmwhz75",
+      "name": "Bouillon Julien",
+      "city": "Paris",
+      "note": "great value for money, Art Nouveau Design",
+      "website": "https://www.bouillon-julien.com",
+      "cuisine": "Classic French,"
+    },
+    {
+      "id": "uag6ryw5mtmwn2dz",
+      "name": "Bar Hotel Sacher",
+      "city": "Salzburg",
+      "note": "Make a reservation, open after concert",
+      "website": "https://www.sacher.com/de/restaurants/sacher-bar/",
+      "cuisine": "Bar"
+    },
+    {
+      "id": "9dcy4nt3mtmwp6o1",
+      "name": "Krimpelstätter Biergarten",
+      "city": "Salzburg",
+      "note": "Closed Sunday and Monday",
+      "website": "https://krimpelstaetter.at",
+      "cuisine": "Biergarten"
+    },
+    {
+      "id": "x7w6d0rtmtmwsp4p",
+      "name": "Gasthof Schloss Aigen",
+      "city": "Salzburg",
+      "note": "20 min taxi ride. Make reservation",
+      "website": "https://www.schloss-aigen.at",
+      "cuisine": "Moden Austrian"
+    },
+    {
+      "id": "y5lvuzhlmtmwvzdo",
+      "name": "Ritzerwirt",
+      "city": "Salzburg",
       "note": "",
-      "website": "",
-      "cuisine": ""
+      "website": "https://www.ritzerwirt.com",
+      "cuisine": "Austrian"
+    },
+    {
+      "id": "hlzi6wqfmtmwxavh",
+      "name": "Zum fidelen Affen",
+      "city": "Salzburg",
+      "note": "Reservation necessary",
+      "website": "https://www.fideleraffe.at",
+      "cuisine": "Austrian"
     }
   ],
   "sights": [],
   "itineraries": [],
-  "updated": "2026-09-02"
+  "updated": "2026-09-04"
 };
